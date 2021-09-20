db5e3f4f844e2d77e9a4e3e7f9b15a710237e36f
Varun Khambhata <khambhata.v@yahoo.com>
2021-09-20 15:19:44 +0530

    Update FastPrimeEvalModule.py

--- Python/FastPrimeEvalModule.py
+++ Python/FastPrimeEvalModule.py
@@ -1,7 +1,7 @@
 # Author:  No One
 # Created: 00.00.0000
 #
-# This program demenstrate nothing.
+# This program demonstrates nothing.
 # The word "Fast" and "Python" cannot come together.
-# If you really want Fast Prime algorithm implemented in Python, do it yourself.
+# If you really want UFPA algorithm implemented in Python, do it yourself.
 # Suck it.
