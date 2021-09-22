5362e8d3e0dcde2f94d75fc2b75d12f7543c7554
Varun Khambhata <khambhata.v@yahoo.com>
2021-09-22 19:09:10 +0530

    Delete FastPrimeEvalModule.py

--- Python/FastPrimeEvalModule.py
+++
@@ -1,7 +0,0 @@
-# Author:  No One
-# Created: 00.00.0000
-#
-# This program demonstrates nothing.
-# The word "Fast" and "Python" cannot come together.
-# If you really want UFPA algorithm implemented in Python, do it yourself.
-# Suck it.
